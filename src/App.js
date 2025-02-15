785eb14e947d7fce16f747e525d921bc09da70df
Talhaoui Oumaima <Oumatlh4@gmail.com>
2025-02-15 22:09:54 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Astraunaut_gaming/components/Home' ; 
-import GamePage from './Astraunaut_gaming/components/GamePage'
+import { Routes, Route } from 'react-router-dom';
+import Home from './Astraunaut_gaming/components/Home';
+import GamePage from './Astraunaut_gaming/components/GamePage';
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/game/:gameId" element={<GamePage />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/game/:gameId" element={<GamePage />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,18 +4,15 @@ import React from 'react';
  import App from './App';
  import reportWebVitals from './reportWebVitals';
  import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './Astraunaut_gaming/Jeu_dés/store';
+
 
  const root = ReactDOM.createRoot(document.getElementById('root'));
  root.render(
-   <React.StrictMode>
+
      <BrowserRouter>
-     <Provider store={store}>
         <App />
-    </Provider>,
      </BrowserRouter>
-   </React.StrictMode>
+
  );
 
  // If you want to start measuring performance in your app, pass a function
